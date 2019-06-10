232bef501696c3ce822555d77b849618ee6e3c0d
apple1 <apple1@macmini2.local>
2019-06-10 19:37:01 +0530

    Flight Result VC added

---
+++ GITHubTest1/FlightResultVC.swift
@@ -0,0 +1,30 @@
+//
+//  FlightResultVC.swift
+//  GITHubTest1
+//
+//  Created by apple1 on 10/06/19.
+//  Copyright © 2019 APPLE. All rights reserved.
+//
+
+import UIKit
+
+class FlightResultVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Flight Result VC")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
